7930c8c3deb213833c6a33b7ab6e709ea1920aa0
Gu,Yu <yugu@ebay.com>
2017-02-16 14:06:01 +0000

    学习require模块

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request,response){
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hello World\n');
-}).listen(8888);
+function start(route) {
+    function onRequest(request,response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("request for " + pathname + " received.");
 
-console.log('Server running at http://127.0.0.1:8888/');
+        route(pathname);
+
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello World\n');
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started!');
+}
+
+exports.start = start;
