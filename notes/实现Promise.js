42cf55b7fadc29be1d9b3dc0141adf82015c2927
guyu <290506494@qq.com>
2019-07-24 02:32:43 +0800

    fix: then的处理

--- notes/实现Promise.js
+++ notes/实现Promise.js
@@ -7,7 +7,7 @@ const isFunction = fn => typeof fn === 'function'
 class MyPromise {
   constructor (handle) {
     if (!isFunction(handle)) {
-      throw new Error('MyPromise hanle is not a function')
+      throw new Error('MyPromise handle is not a function')
     }
     // 状态初始化
     this._status = PENDING
@@ -43,10 +43,13 @@ class MyPromise {
           if (!isFunction(onFulfilled)) {
             onFulfilledNext(value)
           } else {
+            // 如果onFulfilled是一个方法，则根据方法的返回值是否为Promise对象处理
             const res = onFulfilled(value)
             if (res instanceof MyPromise) {
+              // 返回值是Promise对象，则在返回的Promise对象状态改变后执行回调
               res.then(onFulfilledNext, onRejectedNext)
             } else {
+              // 返回值不是Promise，直接作为参数传入下一个then的回调，并立即执行回调函数
               onFulfilledNext(res)
             }
           }
@@ -55,16 +58,32 @@ class MyPromise {
         }
       }
 
+      const rejected = error => {
+        try {
+          if (!isFunction(onRejected)) {
+            onRejectedNext(error)
+          } else {
+            const res = onRejected(error)
+            if (res instanceof MyPromise) {
+              res.then(onFulfilledNext, onRejectedNext)
+            } else {
+              onRejectedNext(res)
+            }
+          }
+        } catch (err) {
+          onRejectedNext(err)
+        }
+      }
       switch (_status) {
         case PENDING:
-          this._fulFilledQueues.push(onFulfilled)
-          this._rejectedQueues.push(onRejected)
+          this._fulFilledQueues.push(fulfilled)
+          this._rejectedQueues.push(rejected)
           break
         case FULFILLED:
-          onFulfilled(_value)
+          fulfilled(_value)
           break
         case REJECTED:
-          onRejected(_value)
+          rejected(_value)
           break
       }
     })
